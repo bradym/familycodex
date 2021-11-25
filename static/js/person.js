0dea4570aab261862594f6e7c1a6cb07695cc43b
Brady Mitchell <bradym@users.noreply.github.com>
2021-11-25 12:40:48 -0700

    Render the first document without requiring a click.

--- static/js/person.js
+++ static/js/person.js
@@ -1,3 +1,18 @@
+function renderPdf(file){
+    var url = '/js/libs/pdfjs/#../../../' + file;
+
+    // Remove existing document viewer
+    $('#documentViewerContainer').empty();
+
+    // Add new document viewer
+    $('<iframe />', {
+        width: '100%',
+        height: '1200px',
+        allowfullscreen: true,
+        src: file
+    }).appendTo('#documentViewerContainer');
+}
+
 $(document).ready(function() {
 
     // Set the first tab active. Done here because it may vary depending on what assets are
@@ -5,29 +20,18 @@ $(document).ready(function() {
     $('#nav-tab a:first-child').addClass('active');
     $('#nav-tabContent div:first-child').addClass('show').addClass('active');
 
-    // This renders the selected PDF from the dropdown
-    $('#documentList').find('a').click(function(event){
+    // Render the first document without requiring a click.
+    if ($('#documentList').children().length == 1) {
+        renderPdf($('#documentList').children().first().data('file'));
+    }
 
+    // Render the selected PDF from the list
+    $('#documentList').find('a').click(function(event){
         event.preventDefault();
 
         // Correctly set the active class
         $('#documentList').find('a').removeClass('active');
         $(this).addClass('active');
-
-        // Get the filename
-        var file = $(this).data('file');
-        var url = '/js/libs/pdfjs/#../../../' + file;
-
-        // Remove existing document viewer
-        $('#documentViewerContainer').empty();
-
-        // Add new document viewer
-        $('<iframe />', {
-            width: '100%',
-            height: '1200px',
-            allowfullscreen: true,
-            src: file
-        }).appendTo('#documentViewerContainer');
-
+        renderPdf($(this).data('file'))
     });
 });
