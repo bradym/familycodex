ad1ddee854f5e76d4fa5b7f2bfba52ea67af96b3
Brady Mitchell <bradym@users.noreply.github.com>
2026-01-02 14:17:48 -0700

    sort family by name

--- bin/generate.py
+++ bin/generate.py
@@ -4,6 +4,7 @@
 import os
 import shutil
 from pathlib import Path
+
 import htmlmin
 import markdown
 import yaml
@@ -11,17 +12,16 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 class GenerateSite:
-
     def __init__(self):
         self.base_dir = os.path.realpath(Path(__file__).parent.parent)
-        self.template_dir = os.path.join(self.base_dir, 'templates')
-        self.data_dir = os.path.join(self.base_dir, 'data')
-        self.build_dir = os.path.join(self.base_dir, 'build')
-        self.static_dir = os.path.join(self.base_dir, 'static')
+        self.template_dir = os.path.join(self.base_dir, "templates")
+        self.data_dir = os.path.join(self.base_dir, "data")
+        self.build_dir = os.path.join(self.base_dir, "build")
+        self.static_dir = os.path.join(self.base_dir, "static")
         self.env = Environment(
-            extensions=['jinja2.ext.debug'],
+            extensions=["jinja2.ext.debug"],
             loader=FileSystemLoader(self.template_dir),
-            autoescape=select_autoescape(['html', 'xml'])
+            autoescape=select_autoescape(["html", "xml"]),
         )
         self.data = {}
         self.html_minify = True
@@ -36,22 +36,22 @@ class GenerateSite:
             os.mkdir(self.build_dir)
 
     def generate_family_pages(self):
-        for item in self.data['family']:
-            dest = Path(self.build_dir).joinpath(f'{item}.html')
-            render_data = {'family': self.data['family'][item], 'nav': self.data['nav']}
-            self.generate_page(render_data, dest, 'family.j2')
+        for item in self.data["family"]:
+            dest = Path(self.build_dir).joinpath(f"{item}.html")
+            render_data = {"family": self.data["family"][item], "nav": self.data["nav"]}
+            self.generate_page(render_data, dest, "family.j2")
 
     def generate_index(self):
         render_data = self.data
-        dest = Path(self.build_dir).joinpath('index.html')
-        self.generate_page(render_data, dest, 'index.j2')
+        dest = Path(self.build_dir).joinpath("index.html")
+        self.generate_page(render_data, dest, "index.j2")
 
     def generate_page(self, data, dest, template):
         html = self.env.get_template(template).render(data)
         if self.html_minify:
             html = htmlmin.minify(html, remove_comments=True)
 
-        with open(dest, 'w') as f:
+        with open(dest, "w") as f:
             f.write(html)
 
     def clean(self):
@@ -60,27 +60,32 @@ class GenerateSite:
             shutil.rmtree(self.build_dir)
             os.mkdir(self.build_dir)
 
-        media_dir = os.path.join(self.base_dir, 'media')
+        media_dir = os.path.join(self.base_dir, "media")
 
         if os.path.isdir(self.build_dir) and os.path.isdir(media_dir):
-            symlink_path = os.path.join(self.build_dir, 'media')
+            symlink_path = os.path.join(self.build_dir, "media")
             os.symlink(media_dir, symlink_path)
 
     def copy_static(self):
         """Copies code in the static folder to the build directory without modification."""
-        for current_dir in [os.path.join(self.base_dir, 'node_modules'), self.static_dir]:
-
+        for current_dir in [
+            os.path.join(self.base_dir, "node_modules"),
+            self.static_dir,
+        ]:
             for root, dirs, files in os.walk(current_dir):
-
                 for item in files:
                     source = os.path.join(root, item)
                     base = os.path.basename(root)
 
-                    if base == 'static':
+                    if base == "static":
                         dest_dir = self.build_dir
-                    elif 'node_modules' in root:
-                        node_module = root.split('node_modules')[1].lstrip('/').split('/')[0]
-                        dest_dir = os.path.join(self.build_dir, node_module, os.path.basename(root))
+                    elif "node_modules" in root:
+                        node_module = (
+                            root.split("node_modules")[1].lstrip("/").split("/")[0]
+                        )
+                        dest_dir = os.path.join(
+                            self.build_dir, node_module, os.path.basename(root)
+                        )
                     else:
                         dest_dir = os.path.join(self.build_dir, os.path.basename(root))
 
@@ -94,45 +99,52 @@ class GenerateSite:
         """
         Loop through families folder and load yaml data and stories into data object.
         """
-        md = markdown.Markdown(extensions=['markdown.extensions.meta'])
-        self.data = {'family': {}, 'nav': []}
+        md = markdown.Markdown(extensions=["markdown.extensions.meta"])
+        self.data = {"family": {}, "nav": []}
 
-        root = Path(f'{self.data_dir}/families')
+        root = Path(f"{self.data_dir}/families")
 
         for current_family_path in root.iterdir():
             current_family = current_family_path.stem
             current_root = root.joinpath(current_family)
-            data_file = current_root.joinpath(f'{current_family}.yaml')
+            data_file = current_root.joinpath(f"{current_family}.yaml")
 
             if not data_file.exists():
                 continue
 
-            with open(data_file, 'r', encoding='utf-8') as f:
+            with open(data_file, "r", encoding="utf-8") as f:
                 current_data = yaml.safe_load(f)
 
-            if not include_fake_data and 'fake' in current_data and current_data['fake']:
+            if (
+                not include_fake_data
+                and "fake" in current_data
+                and current_data["fake"]
+            ):
                 continue
 
-            story_path = current_root.joinpath('stories')
+            story_path = current_root.joinpath("stories")
             if story_path.is_dir():
-                if 'stories' not in current_data:
-                    current_data['stories'] = []
+                if "stories" not in current_data:
+                    current_data["stories"] = []
 
                 for story in story_path.iterdir():
-                    with open(story, 'r', encoding='utf-8') as f:
+                    with open(story, "r", encoding="utf-8") as f:
                         # content = md.convert(f.read())
                         current_story = {
-                            'content': md.convert(f.read()),
-                            'id': story.stem
+                            "content": md.convert(f.read()),
+                            "id": story.stem,
                         }
 
                         for key in md.Meta:
-                            current_story[key] = '\n'.join(md.Meta[key])
+                            current_story[key] = "\n".join(md.Meta[key])
 
-                        current_data['stories'].append(current_story)
+                        current_data["stories"].append(current_story)
 
-            self.data['family'][current_family] = current_data
-            self.data['nav'].append({'href': f'/{current_family}.html', 'text': current_data['name']})
+            self.data["family"][current_family] = current_data
+            self.data["nav"].append(
+                {"href": f"/{current_family}.html", "text": current_data["name"]}
+            )
+        self.data["nav"].sort(key=lambda item: item["text"].casefold())
 
     def set_minify(self, html_minify):
         """Setter for the html_minify property. Used to disable html minification when built via the local server."""
@@ -153,8 +165,8 @@ def run(html_minify=True, include_unpublished_data=False):
     generator.generate_index()
     generator.generate_family_pages()
 
-    print('Site generation complete')
+    print("Site generation complete")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
